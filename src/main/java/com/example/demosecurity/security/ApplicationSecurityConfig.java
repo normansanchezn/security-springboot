278dda2e475ebd522a2526050f11f0d5728d8a56
Norman Sánchez <normansanchezn@gmail.com>
2021-03-25 11:33:32 -0600

    [UPDATE] Setting authorities to users

--- src/main/java/com/example/demosecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demosecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demosecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demosecurity.security.ApplicationUserPermission.*;
 import static com.example.demosecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,9 +30,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_READ.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_READ.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_READ.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,19 +50,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails user = User.builder()
                 .username("anna")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+                //.roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails norman = User.builder()
                 .username("norman")
                 .password(passwordEncoder.encode("password"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails hugo = User.builder()
                 .username("hugo")
                 .password(passwordEncoder.encode("password"))
-                .roles(ADMINTRAINEE.name())
+                //.roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
@@ -63,6 +73,5 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 norman,
                 hugo
         );
-
     }
 }
