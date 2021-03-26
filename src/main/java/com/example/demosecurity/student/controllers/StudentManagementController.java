811d755ee969ad55a9f2ebdc04d41e381802ecb2
Norman Sánchez <normansanchezn@gmail.com>
2021-03-25 20:07:10 -0600

    [UPDATE] uses preAuthorize... antMatchers don't used!

--- src/main/java/com/example/demosecurity/student/controllers/StudentManagementController.java
+++ src/main/java/com/example/demosecurity/student/controllers/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.example.demosecurity.student.controllers;
 
 import com.example.demosecurity.student.model.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,21 +18,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println(String.format("%s %s", studentId, student));
     }
